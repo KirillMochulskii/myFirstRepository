a6df86d5a31769b4377d65a7b3940ea167987e6b
Кирилл <kirilldjaped@gmail.com>
2026-08-23 21:30:32 +0300

    Добавление sout GIT HELLO

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main{
     public static void main(String[] args) {
         System.out.println("Branch KIRILL");
+        List<String> str = new ArrayList<>();
+        for( int i=0;i<10;i++){
+            String value =i+"";
+            str.add(value);
+        }
+        for(String values:str){
+            System.out.print(values+" ");
+        }
+
 
 
 
