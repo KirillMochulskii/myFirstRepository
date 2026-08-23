ca8223dd71eb29a81a8ba8defb59888e50050c84
Кирилл <kirilldjaped@gmail.com>
2026-08-23 21:34:23 +0300

    Добавление while

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Branch DEVELOPER");
+        System.out.println("Branch KIRILL");
+       boolean isDoor = true;
+       int count = 0;
+        while(isDoor){
+            count ++;
+            System.out.println(count);
+            if(count==100){
+                isDoor = false;
+            }
+        }
+
 
 
 
